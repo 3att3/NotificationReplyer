a1b53aac6c0bad5a4b74d67b0c5b3201cd5b2edf
3att3 <alexandros9898@hotmail.com>
2021-07-13 00:27:14 +0300

    UI updates and SharedPreferences Class added

--- app/src/main/java/com/example/notificationreplyer/MainActivity.java
+++ app/src/main/java/com/example/notificationreplyer/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.notificationreplyer;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Space;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     FirebaseUser currentUser;
-    private TextView textViewLoggedOrNot, tvMainWelcome;
+    private TextView textViewLoggedOrNot, tvMainWelcome, tvMdlMat1;
+    private Button btnMdlMatProceed, btnMdlMatBack;
+    private Space spaceMdlMatHorizontal1;
+
+    MaterialCardView matCVMidl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,58 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         textViewLoggedOrNot = findViewById(R.id.textViewLoggedOrNot);
+        tvMdlMat1 = findViewById(R.id.tvMdlMat1);
+        matCVMidl = findViewById(R.id.matCVMidl);
+        btnMdlMatProceed = findViewById(R.id.btnMdlMatProceed);
+        btnMdlMatBack = findViewById(R.id.btnMdlMatBack);
+
+        spaceMdlMatHorizontal1 = findViewById(R.id.spaceMdlMatHorizontal1);
+
+
+
+        ShPref shPref = new ShPref(this);
+
+        Boolean isOpenFirstTime = shPref.getOpenFirstTime();
+        System.out.println("alex99   ---: isOpenFirstTime: " + isOpenFirstTime);
+        if (!isOpenFirstTime){
+            // show user first welcome message with extra data
+            matCVMidl.dispatchWindowVisibilityChanged(View.VISIBLE);
+            tvMdlMat1.setText(R.string.enableNotificationAccess);
+            btnMdlMatBack.setVisibility(View.GONE);
+            spaceMdlMatHorizontal1.setVisibility(View.GONE);
+            btnMdlMatProceed.setText("Proceed");
+        }
+        else{
+            matCVMidl.dispatchWindowVisibilityChanged(View.INVISIBLE);
+        }
+
+
+        // Buttons on click
+
+        btnMdlMatProceed.setOnClickListener(v -> {
+
+            if (btnMdlMatProceed.getText().toString().equals("Open Settings")){
+                // open settings instead from the three dots
+                shPref.setOpenFirstTime(true);
+                Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                startActivity(intent);
+                matCVMidl.setVisibility(View.INVISIBLE);
+            }else {
+                tvMdlMat1.setText(R.string.enableNotificationAccessInstractions);
+                btnMdlMatBack.setVisibility(View.VISIBLE);
+                spaceMdlMatHorizontal1.setVisibility(View.VISIBLE);
+                btnMdlMatProceed.setText("Open Settings");
+            }
+
+        });
+
+        btnMdlMatBack.setOnClickListener(v -> {
+            matCVMidl.dispatchWindowVisibilityChanged(View.VISIBLE);
+            tvMdlMat1.setText(R.string.enableNotificationAccess);
+            btnMdlMatBack.setVisibility(View.GONE);
+            spaceMdlMatHorizontal1.setVisibility(View.GONE);
+            btnMdlMatProceed.setText("Proceed");
+        });
     }
 
     @Override
@@ -69,6 +132,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 }
---
+++ app/src/main/java/com/example/notificationreplyer/ShPref.java
@@ -0,0 +1,46 @@
+package com.example.notificationreplyer;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShPref extends AppCompatActivity{
+
+    private final String SHARED_PREF_NAME = "thisIsASharedPreferenceName",
+            OPEN_FIRST_TIME_KEY = "openFirstTimeKey";
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    Context context;
+
+
+    ShPref(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, 0); // MODE_PRIVATE
+        editor = sharedPreferences.edit();
+    }
+
+    public void setOpenFirstTime(boolean openFirstTime){
+        editor.putBoolean(OPEN_FIRST_TIME_KEY, openFirstTime);
+        editor.commit();
+    }
+
+    public boolean getOpenFirstTime(){
+        return sharedPreferences.getBoolean(OPEN_FIRST_TIME_KEY, false);
+    }
+
+
+    // remove afterwords
+    // ---------------------------------------- Other ------------------------------------------- //
+
+    public void setLessonNumber(int lessonNum){
+        editor.putInt("lessonNumber_", lessonNum);
+        editor.commit();
+    }
+    public int getLessonNumber(){
+        return sharedPreferences.getInt("lessonNumber_", -1);
+    }
+
+
+}
